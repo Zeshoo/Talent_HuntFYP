91b4374718f85f5416235796251735aefc1cea97
Zeshoo <zeshantalat9337@gmail.com>
2025-06-16 22:26:00 +0500

    SAVE allAdd project files.

---
+++ Talent_Hunt/Models/TaskDetailsWithSubmissionsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Talent_Hunt.Models
+{
+    public class TaskDetailsWithSubmissionsViewModel
+    {
+        public string EventTitle { get; set; }
+        public string Description { get; set; }
+        public List<SubmissionDetail> Submissions { get; set; }
+        public int SubmissionID { get; set; }
+    }
+
+    public class SubmissionDetail
+    {
+        public int SubmissionID { get; set; }
+        public string UserName { get; set; }
+        public DateTime SubmissionTime { get; set; }
+        public int? Marks { get; set; }
+        public string PathofSubmission { get; set; }
+    }
+
+
+}
